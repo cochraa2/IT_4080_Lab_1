4fb024a3f599354d703d2d74524cc430d06301dd
cochraa2 <cochraa2@mail.uc.edu>
2023-02-27 18:01:28 -0500

    Added functionality to move multiple cars

--- Assets/Scripts/CarScript.cs
+++ Assets/Scripts/CarScript.cs
@@ -5,10 +5,15 @@ using Unity.Netcode;
 
 public class CarScript : NetworkBehaviour
 {
-    public float speed = 20f;
-    private float turnSpeed = 100f;
-    public float horizontalInput;
-    public float forwardInput;
+    public NetworkVariable<Vector3> PositionChange = new NetworkVariable<Vector3>();
+    public NetworkVariable<Vector3> RotationChange = new NetworkVariable<Vector3>();
+
+
+    private float speed = .02f;
+    private float turnSpeed = .2f;
+    private float horizontalInput;
+    private float forwardInput;
+
     private Camera _camera;
 
     public override void OnNetworkSpawn()
@@ -18,16 +23,49 @@ public class CarScript : NetworkBehaviour
         _camera.enabled = IsOwner;
     }
 
-
     void Update()
     {
-        //Move the car around
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        //Move the cars around
 
-        transform.Translate(Vector3.left * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+        if (IsOwner)
+        {
+            movementInputs();
+            Vector3 goForward = new Vector3(forwardInput, 0, 0);
+            goForward *= speed;
+            Vector3 turnCar = new Vector3(0, horizontalInput, 0);
+            turnCar *= turnSpeed;
+            requestPositionToMoveServerRpc(goForward, turnCar);
+        }
+
+        if (!IsOwner || IsHost)
+        {
+            transform.Translate(PositionChange.Value);
+            transform.Rotate(RotationChange.Value);
+        }
 
 
     }
+
+
+    private void movementInputs()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+    }
+
+    //------------------
+    //RPC
+    //------------------
+
+    [ServerRpc]
+
+    void requestPositionToMoveServerRpc(Vector3 posChange, Vector3 rotChange)
+    {
+        if (!IsServer && !IsHost)
+            return;
+
+        PositionChange.Value = posChange;
+        RotationChange.Value = rotChange;
+    }
+
 }
