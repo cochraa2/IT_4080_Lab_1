b0220763a4848bf0929a4bb14d7edeaa771fdb62
cochraa2 <cochraa2@mail.uc.edu>
2023-04-20 03:08:22 -0400

    Changed files to MonoBehaviour, hoping to fix things soon

--- Assets/Scripts/TrackCheckpoints.cs
+++ Assets/Scripts/TrackCheckpoints.cs
@@ -3,25 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
-public class TrackCheckpoints : NetworkBehaviour
+public class TrackCheckpoints : MonoBehaviour
 {
     private CheckpointScript singleCheckpoint;
 
-    public void Awake()
-    {
-        Transform checkpointGroupTransform = transform.Find(tag = "CheckpointGroup");
-        //Transform checkpointSingleTransform = transform.Find(tag = "Checkpoint");
+    //public void Awake()
+    //{
+    //    Transform checkpointGroupTransform = transform.Find(tag = "CheckpointGroup");
+    //    //Transform checkpointSingleTransform = transform.Find(tag = "Checkpoint");
 
-        foreach(Transform checkpointChildren in checkpointGroupTransform)
-        {
-            CheckpointScript checkpoint = checkpointChildren.GetComponent<CheckpointScript>();
-            Debug.Log($"Checkpoint found:{checkpointChildren} ");
-        }
-    }
+    //    foreach(Transform checkpointChildren in checkpointGroupTransform)
+    //    {
+    //        CheckpointScript checkpoint = checkpointChildren.GetComponent<CheckpointScript>();
+    //        Debug.Log($"Checkpoint found:{checkpointChildren} ");
+    //    }
+    //}
 
-    public void PlayerThroughCheckpoint(CheckpointScript checkpoint)
-    {
-        Debug.Log(checkpoint.transform.name);
-    }
+    //public void PlayerThroughCheckpoint(CheckpointScript checkpoint)
+    //{
+    //    Debug.Log(checkpoint.transform.name);
+    //}
 
 }
